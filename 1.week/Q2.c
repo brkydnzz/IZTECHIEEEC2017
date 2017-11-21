b27d32e07b8ad9a0e0e2584eba4d8d34a180a600
brkydnzz <33721876+brkydnzz@users.noreply.github.com>
2017-11-22 00:35:33 +0300

    Add files via upload

---
+++ 1.week/Q2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+   int number1,number2;
+   printf("Enter the first number.\n");
+   scanf("%d",&number1);
+   printf("Enter the second number.\n");
+   scanf("%d",&number2);
+   printf("First number is %d\n",number1);
+   printf("Second number is %d\n",number2);
+   return 0;
+}
---
+++ 1.week/Q3.2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int number1,number2,result;
+	printf("First number:\n");
+	scanf("%d",&number1);
+	
+	printf("Second number:\n");
+	scanf("%d",&number2);
+	
+	result = number1 * number2;
+	
+	printf("%d*%d=%d",number1,number2,result);
+	return 0;
+}
